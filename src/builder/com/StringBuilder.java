afcde6d2bdcce04448d292a3b7042db390e5ff68
Bharathy12 <bharathymuraj@gmail.com>
2023-02-06 11:55:13 +0530

    changes

---
+++ src/builder/com/StringBuilder.java
@@ -0,0 +1,10 @@
+package builder.com;
+
+public class StringBuilder {
+	
+	public static void main(String[] args) {
+		StringBuilder bb = new StringBuilder("Welcome");
+		System.out.println(bb.main(15));
+	}
+
+}
